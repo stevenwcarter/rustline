b492769a4d7aa0a7cfe1efd14b844dbe3f59581f
Steven Carter <steve@javapl.us>
2026-07-20 18:45:18 -0400

    feat(core): windows widget with current-window emphasis

--- crates/rustline-core/src/widgets/mod.rs
+++ crates/rustline-core/src/widgets/mod.rs
@@ -3,9 +3,11 @@ pub mod datetime;
 pub mod hostname;
 pub mod loadavg;
 pub mod pane_id;
+pub mod windows;
 
 pub use cwd::Cwd;
 pub use datetime::DateTime;
 pub use hostname::Hostname;
 pub use loadavg::LoadAvg;
 pub use pane_id::PaneId;
+pub use windows::Windows;
